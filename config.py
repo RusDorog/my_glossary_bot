e2356fc61092e1c8865e426e1eb9621b71ffd34e
rrn80 <yandex.by>
2026-06-22 21:55:51 +0300

    removed the extra line

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# print(os.environ.get('BOT_TOKEN'))  # должно напечатать ваш токен
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN не задан в .env файле")
